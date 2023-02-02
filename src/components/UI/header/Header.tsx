77699a58aa92644df78abba1aa9a17d1a49b5c41
Egor Dubovik <egorka54896@gmail.com>
2023-02-02 13:14:59 +0300

    refactor: add adaptive for header

--- src/components/UI/header/Header.tsx
+++ src/components/UI/header/Header.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { ISearchProps } from 'types/search';
-import ButtonSwitchMenu from '../buttons/buttonSwitchMenu/ButtonSwitchMenu';
 import Navbar from '../navbar/Navbar';
 import SearchBar from '../searchBar/SearchBar';
 import classes from './Header.module.css';
@@ -18,7 +17,6 @@ class Header extends Component<ISearchProps> {
             <div className={classes.Logo}>RS SChool</div>
             <div className={classes.Menu}>
               <Navbar />
-              <ButtonSwitchMenu />
             </div>
             <SearchBar setSearch={this.props.setSearch} />
           </div>
