a23456e19ae94274f08c6f35e490363f9ba414d6
Egor Dubovik <egorka54896@gmail.com>
2023-03-16 19:07:38 +0300

    refactor: delete extra imports in App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 import userEvent from '@testing-library/user-event';
